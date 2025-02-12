2a60474dd288e5d1215348c63fc7ff99efedf0f1
FAYRESU <136466571+FAYRESU@users.noreply.github.com>
2025-02-12 21:01:59 +0700

    signup_test.py

--- alert_test.py
+++ alert_test.py
@@ -8,7 +8,6 @@ import time
 class GoogleTest(unittest.TestCase):
     
     def setUp(self):
-        # ใช้ raw string เพื่อป้องกันปัญหา escape sequence
         s = Service(r"D:\chromedriver\chromedriver.exe")
         self.driver = webdriver.Chrome(service=s)
         self.driver.get("https://sc.npru.ac.th/sc_shortcourses/signup")
@@ -16,48 +15,48 @@ class GoogleTest(unittest.TestCase):
     def test_fill_form(self):
         driver = self.driver
 
-        # กรอกคำนำหน้าชื่อ
-        name_title = Select(driver.find_element(By.ID, "nameTitleTha"))
-        name_title.select_by_value("นางสาว")  # เลือก "นาย"
 
-        # กรอกชื่อและนามสกุล
+        name_title = Select(driver.find_element(By.ID, "nameTitleTha"))
+        name_title.select_by_value("นางสาว") 
+
+       
         driver.find_element(By.ID, "firstnameTha").send_keys("ณัฐกานต์")
         driver.find_element(By.ID, "lastnameTha").send_keys("จำรัสภูมิ")
 
-        # คำนำหน้าภาษาอังกฤษ
+        
         name_title_eng = Select(driver.find_element(By.ID, "nameTitleEng"))
         name_title_eng.select_by_value("Ms.")
 
-        # กรอกชื่อ-นามสกุล ภาษาอังกฤษ
+        
         driver.find_element(By.ID, "firstnameEng").send_keys("Natthakan")
         driver.find_element(By.ID, "lastnameEng").send_keys("Jamratphum")
 
-        # กรอกวันเดือนปีเกิด
+       
         Select(driver.find_element(By.ID, "birthDate")).select_by_value("26")
         Select(driver.find_element(By.ID, "birthMonth")).select_by_value("12")
         Select(driver.find_element(By.ID, "birthYear")).select_by_value("2004")
 
-        # กรอกข้อมูลส่วนตัวอื่น ๆ
+        
         driver.find_element(By.ID, "idCard").send_keys("1429900541531")
         driver.find_element(By.ID, "password").send_keys("Natthakan678")
         driver.find_element(By.ID, "mobile").send_keys("0821505710")
         driver.find_element(By.ID, "email").send_keys("natthaan57101@gmail.com")
         driver.find_element(By.ID, "address").send_keys("หอพักศรีพยุงเฮ้าส์")
 
-        # เลือกจังหวัด
+       
         Select(driver.find_element(By.ID, "province")).select_by_value("นครปฐม")
 
-        # กรอกเขต/อำเภอ, แขวง/ตำบล และรหัสไปรษณีย์
+       
         driver.find_element(By.ID, "district").send_keys("เมือง")
         driver.find_element(By.ID, "subDistrict").send_keys("วังตะกู")
         driver.find_element(By.ID, "postalCode").send_keys("73000")
 
-        # ติ๊ก checkbox ยอมรับข้อตกลง โดยใช้ JavaScript
+       
         accept_checkbox = driver.find_element(By.XPATH, "//*[@id='accept']")
         if not accept_checkbox.is_selected():
             driver.execute_script("arguments[0].click();", accept_checkbox)
 
-        # พัก 3 วินาทีเพื่อดูผลลัพธ์
+        
         time.sleep(3)
 
     def tearDown(self):
